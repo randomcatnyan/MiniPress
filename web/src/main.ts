acdfc19d42dbfab2d1f6b1d22780c597b1539f5d
ot <>
2026-06-10 16:57:51 +0200

    setup le web js pour du typescript

---
+++ web/src/main.ts
@@ -0,0 +1,12 @@
+
+(async () => {
+    try {
+        // les await sont top level dans les modules
+        const c = await fetch('.');
+        console.log(c);
+
+    } catch (err) {
+        console.error('Erreur : ' + err);
+    }
+
+})()
