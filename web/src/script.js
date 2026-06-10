fd4fa3fcd320b5d6d440137673172cb29563298c
ot <>
2026-06-10 16:18:37 +0200

    début script partie web js

--- web/src/script.js
+++ web/src/script.js
@@ -1,2 +1,8 @@
-"use strict";
+try {
+    // les await sont top level dans les modules
+    const c = await fetch('.');
+    console.log(c);
 
+} catch (err) {
+    console.error('Erreur : ' + err);
+}
