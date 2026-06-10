2b71487e493ab6f751ae3fa95f19da020aef4959
Fahim Meziani <fahim.meziani5@etu.univ-lorraine.fr>
2026-06-10 16:54:28 +0200

    modif

--- core/src/webui/actions/GetCreateCategorieAction.php
+++ core/src/webui/actions/GetCreateCategorieAction.php
@@ -6,12 +6,25 @@ namespace minipress\core\webui\actions;
 use Psr\Http\Message\ServerRequestInterface as Request;
 use Psr\Http\Message\ResponseInterface as Response;
 use minipress\core\webui\providers\CsrfTokenProvider;
+use Slim\Exception\HttpForbiddenException;
+use minipress\core\webui\providers\AuthProvider;   
 use Slim\Views\Twig;
 
 class GetCreateCategorieAction
 {
+    private AuthProvider $authProvider;
+
+    public function __construct()
+    {
+        $this->authProvider = new AuthProvider();
+    }
+    
     public function __invoke(Request $rq, Response $rs, array $args): Response
     {
+        $user = $this->authProvider->getSignedInUser();
+        if (!$user) {
+            throw new HttpForbiddenException($rq, "connecte toi pour voir cette page");
+        }
         $token = (new CsrfTokenProvider())->generate();
         
         $view = Twig::fromRequest($rq);
