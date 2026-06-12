6fb21b664fac388315b29eea52bd4436d3d4b197
nacime <nacime.laghezali3@etu.univ-lorraine.fr>
2026-06-12 08:28:39 +0200

    config.ts et type.ts

---
+++ web/ts/module/articleloader.ts
@@ -0,0 +1,12 @@
+import { API_URL} from './config';
+import { Article } from './types';
+
+
+export async function loadArticles(): Promise<Article[]> {
+    const response = await fetch(`${API_URL}/articles`);
+    if (!response.ok) {
+        throw new Error(`Erreur ${response.status}`);
+    }
+    const data = await response.json();
+    return data as Article[];
+}
---
+++ web/ts/module/type.ts
@@ -0,0 +1,6 @@
+export interface Article {
+    titre: string;
+    cree: string;
+    auteur: string;
+    lien: string;
+}
