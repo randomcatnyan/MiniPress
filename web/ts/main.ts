1a2d53e25c7aba43265033c92c5242ada4b0b1ab
ot <>
2026-06-12 09:25:41 +0200

    rajoute hot reload

--- web/ts/main.ts
+++ web/ts/main.ts
@@ -1,5 +1,15 @@
+
 import { loadArticles } from './module/articleloader';
 import { displayArticle } from './module/ui';
+
+// https://esbuild.github.io/api/#live-reload
+// cette ligne sert à avoir un live reload pendant le dev
+new EventSource('/esbuild').addEventListener('change', () => location.reload())
+
+console.log('ss');
+
+let p = document.querySelector('p');
+if (p) p.textContent = 'aadsdda';
 
 document.addEventListener('DOMContentLoaded', () => {
     loadArticles().then((articles: any) => displayArticle(articles))
