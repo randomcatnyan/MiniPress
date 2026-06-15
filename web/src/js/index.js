79b17eb513decfec5d31aa16cc35299bb264131e
nacime <nacime.laghezali3@etu.univ-lorraine.fr>
2026-06-15 11:01:21 +0200

    css

--- web/src/js/index.js
+++ web/src/js/index.js
@@ -1490,7 +1490,7 @@
               }
               return token;
             }
-            var symbol, preErrorSymbol, state, action, a, r, yyval = {}, p2, len, newState, expected;
+            var symbol, preErrorSymbol, state, action, a, r, yyval = {}, p, len, newState, expected;
             while (true) {
               state = stack[stack.length - 1];
               if (this.defaultActions[state]) {
@@ -1505,9 +1505,9 @@
                 var errStr = "";
                 if (!recovering) {
                   expected = [];
-                  for (p2 in table[state])
-                    if (this.terminals_[p2] && p2 > 2) {
-                      expected.push("'" + this.terminals_[p2] + "'");
+                  for (p in table[state])
+                    if (this.terminals_[p] && p > 2) {
+                      expected.push("'" + this.terminals_[p] + "'");
                     }
                   if (this.lexer.showPosition) {
                     errStr = "Parse error on line " + (yylineno + 1) + ":\n" + this.lexer.showPosition() + "\nExpecting " + expected.join(", ") + ", got '" + (this.terminals_[symbol] || symbol) + "'";
@@ -3758,13 +3758,13 @@
       function randomIntInRange(low, high) {
         return Math.round(low + Math.random() * (high - low));
       }
-      function doQuickSort(ary, comparator, p2, r) {
-        if (p2 < r) {
-          var pivotIndex = randomIntInRange(p2, r);
-          var i = p2 - 1;
+      function doQuickSort(ary, comparator, p, r) {
+        if (p < r) {
+          var pivotIndex = randomIntInRange(p, r);
+          var i = p - 1;
           swap(ary, pivotIndex, r);
           var pivot = ary[r];
-          for (var j = p2; j < r; j++) {
+          for (var j = p; j < r; j++) {
             if (comparator(ary[j], pivot) <= 0) {
               i += 1;
               swap(ary, i, j);
@@ -3772,7 +3772,7 @@
           }
           swap(ary, i + 1, j);
           var q = i + 1;
-          doQuickSort(ary, comparator, p2, q - 1);
+          doQuickSort(ary, comparator, p, q - 1);
           doQuickSort(ary, comparator, q + 1, r);
         }
       }
@@ -5874,15 +5874,36 @@
   }
 
   // ts/main.ts
-  var hotReload = new EventSource("/esbuild") ?? null;
-  hotReload?.addEventListener("change", () => location.reload());
-  var p = document.querySelector("p");
-  if (p)
-    p.textContent = "aadsdda";
-  document.addEventListener("DOMContentLoaded", async () => {
-    loadArticles().then((articles) => displayArticle(articles));
+  new EventSource("/esbuild")?.addEventListener("change", () => location.reload());
+  document.addEventListener("DOMContentLoaded", () => {
+    let tousArticles = [];
+    loadArticles().then((articles) => {
+      tousArticles = articles;
+      displayArticle(articles);
+    });
     loadCategories().then((categories) => displayCategories(categories));
-    tri();
+    const recherche = document.getElementById("recherche");
+    recherche.addEventListener("input", () => {
+      const titre = recherche.value.toLowerCase();
+      const filtres = tousArticles.filter(
+        (a) => a.titre.toLowerCase().includes(titre)
+      );
+      displayArticle(filtres);
+    });
   });
+  tri();
+  for (const auteurName of document.querySelectorAll(".auteurName")) {
+    if (auteurName instanceof HTMLElement) {
+      auteurName.addEventListener("click", (e) => {
+        const url = `/api/auteurs/${auteurName.dataset.id}/articles`;
+        const auteur = fetch(url).then((r) => r.json()).then((r) => {
+          const auteurDetails = document.createElement("p");
+          auteurName.parentElement?.parentElement?.appendChild(auteurDetails);
+        }).catch((err) => {
+          console.error("Erreur : " + err);
+        });
+      });
+    }
+  }
 })();
 //# sourceMappingURL=index.js.map
