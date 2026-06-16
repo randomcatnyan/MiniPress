0022613d77c0be19697febe6e7e855d5bb26815f
Fahim Meziani <fahim.meziani5@etu.univ-lorraine.fr>
2026-06-16 10:00:15 +0200

    question 1

---
+++ app/minipress/lib/providers/article_provider.dart
@@ -0,0 +1,8 @@
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+import '../models/article.dart';
+import '../services/api_service.dart';
+
+final articlesProvider = FutureProvider<List<Article>>((ref) async {
+  final api = ApiService();
+  return api.getArticles();
+});
